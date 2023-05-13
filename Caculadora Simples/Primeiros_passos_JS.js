96fac2cb002371bc82195a59a687b5c5356e0c64
henriquezxc <henriqueverisimop@gmail.com>
2023-05-13 00:17:38 -0300

    Meu primeiro exercicio em Js

---
+++ Caculadora Simples/Primeiros_passos_JS.js
@@ -0,0 +1,15 @@
+// 2 Entradas de numeros inteiros para verificar quem e o maior dos 2.
+
+function Soma() {
+    let entrada_1 = parseInt(document.getElementById('soma').value);
+    let entrada_2 = parseInt(document.getElementById('soma2').value);
+    resultado = (entrada_1 + entrada_2)
+    alert(resultado)
+    }
+
+function Multiplicar() {
+    let entrada_3 = parseInt(document.getElementById('soma').value);
+    let entrada_4 = parseInt(document.getElementById('soma2').value);
+    alert(entrada_3 * entrada_4);
+}
+
---
+++ Caculadora Simples/README.md
@@ -0,0 +1,2 @@
+# Exercicio_JavaScript
+Meus Exercicios - Iniciante
