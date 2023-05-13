a0964ab85511e59e496764deb0165541f561b318
henriquezxc <henriqueverisimop@gmail.com>
2023-05-13 15:58:33 -0300

    Mensagem quando não tinha numeros nas caixas de entrada, fizemos o tratamento desses erro

--- Caculadora Simples/Primeiros_passos_JS.js
+++ Caculadora Simples/Primeiros_passos_JS.js
@@ -1,17 +1,34 @@
-// 2 Entradas de numeros inteiros para verificar quem e o maior dos 2.
 
 function Soma() {
-    let entrada_1 = parseInt(document.getElementById('soma').value);
-    let entrada_2 = parseInt(document.getElementById('soma2').value);
-    Resultado_Soma = (entrada_1 + entrada_2)
-    document.getElementById('resultado').value = Resultado_Soma;
+    var entrada_1 = parseInt(document.getElementById('soma').value);
+    var entrada_2 = parseInt(document.getElementById('soma2').value);
+    Resultado_Soma = (entrada_1 + entrada_2);
+    // Usei o ( isNAN ) para verificar se e falso ou verdadeiro a entrada
+    Tratamento_1 = isNaN(Resultado_Soma);
+    // Condição para caso não tenha inserido nenhum numero
+    if (Tratamento_1 === false) {
+        document.getElementById('resultado').value = Resultado_Soma;
+    } else {
+        // Impressão da mensagem de erro
+        document.getElementById('resultado').value = '  Sem valores para Calcular';
     }
+}
+
 
 function Multiplicar() {
-    let entrada_3 = parseInt(document.getElementById('soma').value);
-    let entrada_4 = parseInt(document.getElementById('soma2').value);
+    var entrada_3 = parseInt(document.getElementById('soma').value);
+    var entrada_4 = parseInt(document.getElementById('soma2').value);
     Resultado_Multiplicar = (entrada_3 * entrada_4)
-    document.getElementById('resultado').value = Resultado_Multiplicar;
+    // Usei o ( isNAN ) para verificar se e falso ou verdadeiro a entrada
+    Tratamento_2 = isNaN(Resultado_Multiplicar);
+    // Condição para caso não tenha inserido nenhum numero
+    if (Tratamento_2 === false) {
+        document.getElementById('resultado').value = Resultado_Multiplicar;
+    } else {
+        // Impressão da mensagem de erro
+        document.getElementById('resultado').value = '  Sem valores para Calcular';
+    }
+
 }
 function Limpar() {
     alert('Funcionalidade em desenvolvimento...');
