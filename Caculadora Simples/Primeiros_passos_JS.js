7af842e5f37aa1a36893d715d99f9a63bfc754b6
henriquezxc <henriqueverisimop@gmail.com>
2023-05-13 21:36:11 -0300

    Add função dividir

--- Caculadora Simples/Primeiros_passos_JS.js
+++ Caculadora Simples/Primeiros_passos_JS.js
@@ -1,54 +1,65 @@
-
 function Soma() {
-    var entrada_1 = parseInt(document.getElementById('soma').value);
-    var entrada_2 = parseInt(document.getElementById('soma2').value);
-    Resultado_Soma = (entrada_1 + entrada_2);
-    // Usei o ( isNAN ) para verificar se e falso ou verdadeiro a entrada
-    Tratamento_1 = isNaN(Resultado_Soma);
-    // Condição para caso não tenha inserido nenhum numero
-    if (Tratamento_1 === false) {
-        document.getElementById('resultado').value = Resultado_Soma;
-    } else {
-        // Impressão da mensagem de erro
-        document.getElementById('resultado').value = '  Sem valores para Calcular';
-    }
+  var entrada_1 = parseFloat(document.getElementById("soma").value);
+  var entrada_2 = parseFloat(document.getElementById("soma2").value);
+  Resultado_Soma = entrada_1 + entrada_2;
+  // Usei o ( isNAN ) para verificar se e falso ou verdadeiro a entrada
+  Tratamento_1 = isNaN(Resultado_Soma);
+  // Condição para caso não tenha inserido nenhum numero
+  if (Tratamento_1 === false) {
+    document.getElementById("resultado").value = Resultado_Soma;
+  } else {
+    // Impressão da mensagem de erro
+    document.getElementById("resultado").value = "  Sem valores para Calcular";
+  }
 }
 
-
 function Multiplicar() {
-    var entrada_3 = parseInt(document.getElementById('soma').value);
-    var entrada_4 = parseInt(document.getElementById('soma2').value);
-    Resultado_Multiplicar = (entrada_3 * entrada_4)
-    // Usei o ( isNAN ) para verificar se e falso ou verdadeiro a entrada
-    Tratamento_2 = isNaN(Resultado_Multiplicar);
-    // Condição para caso não tenha inserido nenhum numero
-    if (Tratamento_2 === false) {
-        document.getElementById('resultado').value = Resultado_Multiplicar;
-    } else {
-        // Impressão da mensagem de erro
-        document.getElementById('resultado').value = '  Sem valores para Calcular'
-    };
+  var entrada_3 = parseFloat(document.getElementById("soma").value);
+  var entrada_4 = parseFloat(document.getElementById("soma2").value);
+  Resultado_Multiplicar = entrada_3 * entrada_4;
+  // Usei o ( isNAN ) para verificar se e falso ou verdadeiro a entrada
+  Tratamento_2 = isNaN(Resultado_Multiplicar);
+  // Condição para caso não tenha inserido nenhum numero
+  if (Tratamento_2 === false) {
+    document.getElementById("resultado").value = Resultado_Multiplicar;
+  } else {
+    // Impressão da mensagem de erro
+    document.getElementById("resultado").value = "  Sem valores para Calcular";
+  }
 }
 
 function Subtrair() {
-    var entrada_5 = parseInt(document.getElementById('soma').value);
-    var entrada_6 = parseInt(document.getElementById('soma2').value);
-    Resultado_Subtrair = (entrada_5 - entrada_6)
-    // Usei o ( isNAN ) para verificar se e falso ou verdadeiro a entrada
-    Tratamento_3 = isNaN(Resultado_Subtrair);
-    // Condição para caso não tenha inserido nenhum numero
-    if (Tratamento_3 === false) {
-        document.getElementById('resultado').value = Resultado_Subtrair;
-    } else {
-        // Impressão da mensagem de erro
-        document.getElementById('resultado').value = '  Sem valores para Calcular'
-    }
-
+  var entrada_5 = parseFloat(document.getElementById("soma").value);
+  var entrada_6 = parseFloat(document.getElementById("soma2").value);
+  Resultado_Subtrair = entrada_5 - entrada_6;
+  // Usei o ( isNAN ) para verificar se e falso ou verdadeiro a entrada
+  Tratamento_3 = isNaN(Resultado_Subtrair);
+  // Condição para caso não tenha inserido nenhum numero
+  if (Tratamento_3 === false) {
+    document.getElementById("resultado").value = Resultado_Subtrair;
+  } else {
+    // Impressão da mensagem de erro
+    document.getElementById("resultado").value = "  Sem valores para Calcular";
+  }
 }
 
+function Dividir() {
+  var entrada_7 = parseFloat(document.getElementById("soma").value);
+  var entrada_8 = parseFloat(document.getElementById("soma2").value);
+  Resultado_Dividir = entrada_7 / entrada_8;
+  Tratamento_4 = isNaN(Resultado_Dividir);
+
+  if (Tratamento_4 === false) {
+    document.getElementById("resultado").value = Resultado_Dividir;
+  } else {
+    // Impressão da mensagem de erro
+    document.getElementById("resultado").value = "  Sem valores para Calcular";
+  }
+}
+infi
 function Limpar() {
-    // Essa funcão pega as 2 entradas e a entrada de resultado e coloca tudo com ('') para ficar vazia
-    document.getElementById('resultado').value = '';
-    document.getElementById('soma').value = '';
-    document.getElementById('soma2').value = '';
+  // Essa funcão pega as 2 entradas e a entrada de resultado e coloca tudo com ('') para ficar vazia
+  document.getElementById("resultado").value = "";
+  document.getElementById("soma").value = "";
+  document.getElementById("soma2").value = "";
 }
